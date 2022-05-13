86e3572e638e662679231d8a77e588feb7de46d2
xSlendiX <gamingxslendix@gmail.com>
2022-05-13 22:24:17 +0300

    Add verboser error messages

--- src/Servers/MainServer.cpp
+++ src/Servers/MainServer.cpp
@@ -151,8 +151,12 @@ void Main::Stop()
     m_running = false;
 }
 
-const string STATUS_OK = "OK\r\n";
-const string STATUS_ERR = "ERR\r\n";
+const string STATUS_ACK = "ACK\r\n";
+const string STATUS_ERR_UNKNOWN_COMMAND = "ERR UNK_CMD\r\n";
+const string STATUS_ERR_INVALID_OPTIONS = "ERR INV_OPT\r\n";
+const string STATUS_ERR_OPTION_UNAVAILABLE = "ERR UNV\r\n";
+const string STATUS_ERR_UNKNOWN = "ERR UNK\r\n";
+const string STATUS_ERR_MOTOR_FAIL = "ERR MOT_FAIL\r\n";
 
 Main::Response Main::ParseInstruction(string instruction)
 {
@@ -172,50 +176,50 @@ Main::Response Main::ParseInstruction(string instruction)
             continue;
 
         if (tokens[0] == "PING" || tokens[0] == "P") {
-            return { ResponseAction::None, STATUS_OK };
+            return { ResponseAction::None, STATUS_ACK };
         } else if (tokens[0] == "CLOSE" || tokens[0] == "EXIT" || tokens[0] == "QUIT" || tokens[0] == "Q") {
-            return { ResponseAction::Close, STATUS_OK };
+            return { ResponseAction::Close, STATUS_ACK };
         } else if (tokens[0] == "MOTOR" || tokens[0] == "M") {
             if (tokens.size() < 3)
-                return { ResponseAction::None, STATUS_ERR };
+                return { ResponseAction::None, STATUS_ERR_INVALID_OPTIONS };
 
             if (tokens[1] == "LEFT" || tokens[1] == "L") {
                 try {
                     motor_left->SetSpeed(stoi(tokens[2]) * (Configuration::the().motors()->left_inverted ? -1 : 1));
                 } catch (...) {
-                    return { ResponseAction::None, STATUS_ERR };
+                    return { ResponseAction::None, STATUS_ERR_MOTOR_FAIL };
                 }
             } else if (tokens[1] == "RIGHT" || tokens[1] == "R") {
                 try {
                     motor_right->SetSpeed(stoi(tokens[2]) * (Configuration::the().motors()->right_inverted ? -1 : 1));
                 } catch (...) {
-                    return { ResponseAction::None, STATUS_ERR };
+                    return { ResponseAction::None, STATUS_ERR_MOTOR_FAIL };
                 }
             } else {
-                return { ResponseAction::None, STATUS_ERR };
+                return { ResponseAction::None, STATUS_ERR_INVALID_OPTIONS };
             }
 
-            return { ResponseAction::None, STATUS_OK };
+            return { ResponseAction::None, STATUS_ACK };
         } else if (Configuration::the().gui_present() && (tokens[0] == "FACE" || tokens[0] == "F")) {
             if (tokens.size() < 2)
-                return { ResponseAction::None, STATUS_ERR };
+                return { ResponseAction::None, STATUS_ERR_INVALID_OPTIONS };
             
             try {
                 int face = stoi(tokens[1]);
                 if (face < 0)
-                    return { ResponseAction::None, STATUS_ERR };
+                    return { ResponseAction::None, STATUS_ERR_INVALID_OPTIONS };
                 Messaging::SendMessage("face " + tokens[1]);
             } catch (...) {
-                return { ResponseAction::None, STATUS_ERR };
+                return { ResponseAction::None, STATUS_ERR_INVALID_OPTIONS };
             }
         } else {
-            return { ResponseAction::None, STATUS_ERR };
+            return { ResponseAction::None, STATUS_ERR_UNKNOWN_COMMAND };
         }
     }
 
     return {
         ResponseAction::None,
-        STATUS_OK
+        STATUS_ACK
     };
 }
 
