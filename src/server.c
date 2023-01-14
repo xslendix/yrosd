e3a8105423d5935b7d25a2ea2dc806f614b68a10
xSlendiX <slendi@socopon.com>
2023-01-15 01:30:57 +0200

    Create main server.
    This commit implements finally the main server and it's necessary
    architecture.
    
    Signed-off-by: xSlendiX <slendi@socopon.com>

--- src/server.c
+++ src/server.c
@@ -1,6 +1,9 @@
 #include "server.h"
-#include "src/logging.h"
 
+#include "src/logging.h"
+#include "src/yrosd.h"
+
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -37,11 +40,11 @@ parse_message(int i, char *msg, client_data_t *data, int sock)
   char *argv[10], *cur = strtok(msg, delim);
   for (;;) {
     cur = strtok(nullptr, delim);
-    if (*cur == '\0')
-      continue;
-
     if (cur == nullptr)
       break;
+
+    if (*cur == '\0')
+      continue;
 
     argv[argc] = cur;
     if (argc >= 9) {
@@ -68,77 +71,120 @@ main_server_loop(void *data)
   client_data_t c_data[FD_SETSIZE] = {{
     .mode = MODE_AUTH,
   }};
-  fd_set fds, readfds;
-  int i, client_addr_len;
-  int client_sock[2], rc, num_socks = 0, max_socks = 2;
 
-  int server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (server_sock == -1)
-    LOG_MSG(LOG_FATAL, "Socket");
+  int opt = true;
+  int master_socket, addrlen, new_socket, client_socket[30],
+      max_clients = 30, activity, i, valread, sd;
+  int max_sd;
+  struct sockaddr_in address;
 
-  struct sockaddr_in server_addr, client_addr;  
-  memset(&server_addr, 0, sizeof(struct sockaddr_in));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons(6782);
+  char buffer[1025];
+  fd_set readfds;
 
-  if (-1 == bind(server_sock, (struct sockaddr *)&server_addr, 
-                 sizeof(struct sockaddr_in))) 
-    LOG_MSG(LOG_FATAL, "Bind");
+  const char message[20];
+  snprintf((char *)message, 20, "YROSauth%s\r\n", app.version);
 
-  if (-1 == listen(server_sock, SOMAXCONN))
-    LOG_MSG(LOG_FATAL, "Listen");
+  for (i = 0; i < max_clients; i++)
+    client_socket[i] = 0;
 
-  FD_ZERO(&fds);
-  FD_SET(server_sock, &fds);
+  if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    LOG_MSG(LOG_FATAL, "Main server: socket error: %s", strerror(errno))
+    return nullptr;
+  }
 
-  while(1) {
-    readfds = fds;
-    rc = select(FD_SETSIZE, &readfds, NULL, NULL, NULL);
+  if (setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char*)&opt, sizeof(opt)) < 0) {
+    LOG_MSG(LOG_FATAL, "Main server: setsockopt error: %s", strerror(errno))
+    return nullptr;
+  }
 
-    if (rc == -1) {
-      LOG_MSG(LOG_FATAL, "Select");
-      break;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(app.running_port);
+
+  if (bind(master_socket, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    LOG_MSG(LOG_FATAL, "Main server: Bind error: %s", strerror(errno))
+    return nullptr;
+  }
+
+  if (listen(master_socket, 3) < 0) {
+    LOG_MSG(LOG_FATAL, "Main server: Listen error: %s", strerror(errno))
+    return nullptr;
+  }
+
+  addrlen = sizeof(address);
+
+  for (;;) {
+    FD_ZERO(&readfds);
+
+    FD_SET(master_socket, &readfds);
+    max_sd = master_socket;
+
+    for (i = 0; i < max_clients; i++) {
+      sd = client_socket[i];
+
+      if (sd > 0)
+        FD_SET(sd, &readfds);
+
+      if (sd > max_sd)
+        max_sd = sd;
     }
 
-    for (i = 0; i < FD_SETSIZE; i++) {
-      if (FD_ISSET(i, &readfds)) {
-        if (i == server_sock) {
-          if (num_socks < max_socks) {
-            client_sock[num_socks] = accept(server_sock,
-                                          (struct sockaddr *) &client_addr,
-                                          (socklen_t *) &client_addr_len);
-            if (client_sock[num_socks] == -1)
-              LOG_MSG(LOG_FATAL, "Accept");
-            FD_SET(client_sock[num_socks], &fds);
-            num_socks++;
-          } else {
-            LOG_MSG(LOG_DEBUG, "Ran out of socket space.");
-          }
+    activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
+
+    if ((activity < 0) && (errno != EINTR))
+      LOG_MSG(LOG_FATAL, "Main server: Select error: %s", strerror(errno));
+
+    if (FD_ISSET(master_socket, &readfds)) {
+      if ((new_socket = accept(master_socket, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        LOG_MSG(LOG_FATAL, "Main server: Accept error: %s", strerror(errno));
+        return nullptr;
+      }
+
+      LOG_MSG(LOG_INFO, "Main server: New connection: %s:%d", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+      if (send(new_socket, message, strlen(message), 0) != strlen(message)) {
+        LOG_MSG(LOG_ERROR, "Main server: Failed sending welcome message: %s", strerror(errno));
+        return nullptr;
+      }
+
+      for (i = 0; i < max_clients; i++) {
+        if (client_socket[i] == 0) {
+          client_socket[i] = new_socket;
+          break;
+        }
+      }
+    }
+
+    for (i = 0; i < max_clients; i++) {
+      sd = client_socket[i];
+
+      if (FD_ISSET(sd, &readfds)) {
+        if ((valread = read(sd, buffer, 1024)) == 0) {
+        sock_disconnect:
+          getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+          LOG_MSG(LOG_INFO, "Main server: Host disconnected: %s:%d", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+          close(sd);
+          client_socket[i] = 0;
         } else {
-          int const message_len = 1024;
-          char message[message_len+1];
-          int in, index = 0, limit = message_len+1;
-
-          while ((in = recv(client_sock[i], &message[index], limit, 0)) > 0) {
-            index += in;
-            limit -= in;
-          }
-
-          parse_message(i, message, &c_data[i], client_sock[i]);
-          LOG_MSG(LOG_DEBUG, "%d - `%s`", index, message);
+          buffer[valread] = '\0';
+          parse_message(i, buffer, &c_data[i], sd);
+          /* FIXME: Remove this if unused.
+          if (response.action == ResponseAction::Close)
+            goto sock_disconnect;
+          */
         }
       }
     }
   }
 
-  close(server_sock);
   return 0;
 }
 
 pthread_t *
 start_main_server(void)
 {
+  LOG_MSG(LOG_DEBUG, "Starting main server port %d.", app.running_port);
   pthread_create(&thread_main, nullptr, &main_server_loop, nullptr);
   return &thread_main;
 }
